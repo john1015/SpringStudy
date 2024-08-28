cc5ce546b4ff932ba561d1db1b1ed64c6b120ffd
john1015 <10151404john@gmail.com>
2024-08-28 12:44:31 +0900

    0828

---
+++ SpringBasicPrioject_DI_12/src/main/java/com/sist/vo/FoodVO.java
@@ -0,0 +1,28 @@
+package com.sist.vo;
+
+import lombok.Data;
+
+/*
+ *  FNO                                       NOT NULL NUMBER(38)
+	 NAME                                               VARCHAR2(900)
+	 TYPE                                               VARCHAR2(900)
+	 PHONE                                              VARCHAR2(100)
+	 ADDRESS                                            VARCHAR2(2000)
+	 SCORE                                              NUMBER(2,1)
+	 THEME                                              VARCHAR2(2000)
+	 POSTER                                             VARCHAR2(2000)
+	 IMAGES                                             VARCHAR2(2000)
+	 TIME                                               VARCHAR2(1000)
+	 PARKING                                            VARCHAR2(2000)
+	 CONTENT                                            VARCHAR2(2000)
+	 RDAYS                                              VARCHAR2(100)
+	 JJIMCOUNT                                          NUMBER(38)
+	 LIKECOUNT                                          NUMBER(38)
+	 HIT                                                NUMBER(38)
+ */
+@Data
+// 사용자 정의 데이터형 => 스프링에 등록대상이 아니다
+public class FoodVO {
+	private int fno;
+	private String name, type , address , time , parking , theme,phone;
+}
