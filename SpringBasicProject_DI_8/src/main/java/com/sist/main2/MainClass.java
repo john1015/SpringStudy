147a924b3920a015de76733c05fa4aeaff874ff2
john1015 <10151404john@gmail.com>
2024-08-28 13:44:26 +0900

    0827

---
+++ SpringBasicProject_DI_8/src/main/java/com/sist/main2/MainClass.java
@@ -0,0 +1,22 @@
+package com.sist.main2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ApplicationContext app=
+        	 new ClassPathXmlApplicationContext("app1.xml");
+        EmpDAO dao=app.getBean("dao",EmpDAO.class);
+        List<EmpVO> list=dao.empListData();
+        for(EmpVO vo:list)
+        {
+        	System.out.println(vo.getEname()+" "
+        			+vo.getJob()+" "
+        			+vo.getDbday());
+        }
+	}
+
+}
